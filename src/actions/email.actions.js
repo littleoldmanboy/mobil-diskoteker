6729ead36d7e64035e26e7d62822d6b8a35a8c93
littleoldmanboy <eaadknr@students.eaaa.dk>
2024-05-28 12:23:38 +0200

    feat: Comments

--- src/actions/email.actions.js
+++ src/actions/email.actions.js
@@ -1,11 +1,14 @@
 "use server";
 
+// Import the Resend module and the email templates for user and owner confirmations
 import { Resend } from "resend";
 import ConfirmationUser from "../../react-email-starter/emails/confirmation-user";
 import ConfirmationOwner from "../../react-email-starter/emails/confirmation-owner";
 
+// Initialize the Resend client with the API key from environment variables
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+// Function to send a confirmation email to the user
 export async function sendUserConfirmationEmail({
   title,
   locations,
@@ -15,6 +18,7 @@ export async function sendUserConfirmationEmail({
   link,
   email,
 }) {
+  // Use the Resend client to send an email with the given details
   const { data, error } = await resend.emails.send({
     from: "Mobil-Diskoteker.dk <mobildiskotekerne@dennisrussell.dk>",
     to: [email],
@@ -29,11 +33,13 @@ export async function sendUserConfirmationEmail({
     }),
   });
 
+  // Log an error if the email fails to send
   if (error) {
     console.error(error);
     return "error";
   }
 
+  // Return "success" if the email sends successfully, otherwise "error"
   if (data) {
     return "success";
   }
@@ -41,6 +47,7 @@ export async function sendUserConfirmationEmail({
   return "error";
 }
 
+// Function to send a confirmation email to the owner/admin
 export async function sendOwnerConfirmationEmail({
   title,
   locations,
@@ -50,6 +57,7 @@ export async function sendOwnerConfirmationEmail({
   link,
   email,
 }) {
+  // Use the Resend client to send an email with the given details
   const { data, error } = await resend.emails.send({
     from: "Mobil-Diskoteker.dk <mobildiskotekerne@dennisrussell.dk>",
     to: ["dennis.g.leerup@hotmail.com"],
@@ -65,11 +73,13 @@ export async function sendOwnerConfirmationEmail({
     }),
   });
 
+  // Log an error if the email fails to send
   if (error) {
     console.error(error);
     return "error";
   }
 
+  // Return "success" if the email sends successfully, otherwise "error"
   if (data) {
     return "success";
   }
